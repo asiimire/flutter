60f885fa09f70d998beb1aef899fe6b3e0ef7d57
Asiimire Patricia <101188715+asiimire@users.noreply.github.com>
2024-09-19 13:24:29 +0000

    oop in dart

---
+++ oop.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  Contact c1 = Contact();
+  c1.first_name = "Patricia";
+  c1.last_name = "Asiimire";
+  c1.phone = "0789999222";
+  c1.address = "Kampala";
+  c1.age = 22;
+  c1.weight = 55.0;
+
+
+  print("First Name: ${c1.first_name}")
+  print("Last Name: ${c1.last_name}")
+  print("Phone: ${c1.phone}")
+  print("Age: ${c1.age}")
+  print("Address: ${c1.address}")
+  print("Weight: ${c1.weight}")
+}
+
+class Contact {
+  String first_name = "";
+  String last_name = "";
+  String phone = "";
+  String address = "";
+  int age = "";
+  double weight = 0.0;
+}
