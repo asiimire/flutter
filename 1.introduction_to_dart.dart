936dc709478d8d7e0780245e3b04a0d53226a228
patriciaasiimire <zoeqwiggly@gmail.com>
2024-09-21 10:17:38 +0300

    learning dart

--- 1.introduction_to_dart.dart
+++ 1.introduction_to_dart.dart
@@ -1,3 +1,14 @@
 main() {
   print('hello dart');
+
+   // Strings
+    String name = 'John Doe';
+    print('Hello, $name!');
 }
+// data types in dart
+    // Numbers
+    // Strings
+    // Booleans
+    // Lists
+    // Maps
+    // dynamic type
---
+++ 2.variables.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  // Strings
+  String name = 'Asiimire Patricia';
+  int age = 22;
+  double weight = 55.0;
+  print('Hello, $name you are $age and you weigh $weight kg!');
+}
