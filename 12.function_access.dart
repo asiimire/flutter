2ad43e9a12f832816699e0a0ef967a9b2dc6f7a1
patriciaasiimire <zoeqwiggly@gmail.com>
2024-09-21 14:48:56 +0300

    learning dart

--- 12.function_access.dart
+++ 12.function_access.dart
@@ -2,6 +2,35 @@ String name = "Asiimire"; // global variable
 void main(List<String> args) {
   f1();
   f2();
+  save_contact(name: 'Asiimire', phone: '+25678912345');
+}
+
+save_contact({
+  required String name,
+  String? last_name,
+  String other_name = "No other name",
+  required String phone,
+  String? email,
+  String? address,
+}) {
+  // implementation of the save_contact function
+  Map<String, dynamic> data = {};
+  data["name"] = name;
+  if (last_name == null) {
+    data["last_name"] = 'No last name';
+  } else {
+    data["last_name"] = last_name;
+  }
+
+  print("Name: $name");
+  print("Other name: $other_name");
+  print("Phone: $phone");
+  if (email != null) {
+    print("Email: $email");
+  }
+  if (address != null) {
+    print("Address: $address");
+  }
 }
 
 f1() {
